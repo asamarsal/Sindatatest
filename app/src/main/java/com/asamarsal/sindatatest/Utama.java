0d923e0b1b757445905bf73bfd7d0b71d2018fc1
ujangmental <asamarsal@gmail.com>
2024-06-08 07:52:01 +0700

    Activity Pindah

--- app/src/main/java/com/asamarsal/sindatatest/Utama.java
+++ app/src/main/java/com/asamarsal/sindatatest/Utama.java
@@ -6,21 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class Utama extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_utama);
 
-        ImageButton btn_back = findViewById(R.id.btn_back);
+        Button buttontekan = findViewById(R.id.buttontekan);
 
-        btn_back.setOnClickListener(new View.OnClickListener() {
+        buttontekan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                Intent intent = new Intent(Utama.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
